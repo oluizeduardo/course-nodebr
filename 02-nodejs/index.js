5b14f71f3cc202ea8206f923ad8bbad5e1ae4a98
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2020-04-26 16:05:20 +0100

    Updating works with callbacks.

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -1,49 +1,56 @@
 // Get a user.
 // Get user's phone from the user's id.
-// Get user's address from the user's id.
+// Get user's address from the user's phone.
 
 function getUser(callback) {
     setTimeout(() => {
+        // The first parameter is the error.
       return callback(null, {
         id: 1,
-        name: "Luiz Costa",
-        birthday: new Date()
+        name: "Luiz Eduardo Costa",
+        birthday: new Date('1993/05/24').getFullYear(),
+        age: (new Date().getFullYear() - new Date('1993/05/24').getFullYear())
       });
     }, 1000);
   }
-  
+
+  // Returns the user's phone based on the user's id.
   function getPhone(idUser, callback) {
     setTimeout(() => {
-      return callback(null, {
-        phone: '3547246',
-        ddd: 83,
-      });
+      if(idUser){// Checks if it's a valid id.        
+        return callback(null, {
+            phone: '354-7246',
+            ddd: 83,
+        });
+      }else
+        return;
     }, 500);
   }
   
-  function getAddress(idUser, callback) {
+  // Returns the user's address based on the user's phone.
+  function getAddress(idPhone, callback) {
     setTimeout(() => {
       return callback(null, {
-        street: "Dos bobos",
+        street: "Howth Road",
         number: 230,
       }, 300);
     });
   }
   
   
-getUser(function resolvernewUser(error, newUser) {
+getUser(function resolveNewUser(error, newUser) {
     if (error) {
       console.log("Error! ", error);
       return;
     }
   
-    getPhone(newUser.id, function resolverphone(error1, phone) {
+    getPhone(newUser.id, function resolvePhone(error1, phone) {
       if (error1) {
         console.log("Error!", error1);
         return;
       }
   
-      getAddress(newUser.id, function resolverEndereco(error2, address) {
+      getAddress(newUser.phone, function resolveAddress(error2, address) {
         if (error2) {
           console.log("Error!", error2);
           return;
@@ -52,7 +59,9 @@ getUser(function resolvernewUser(error, newUser) {
         console.log(`
         Name:${ newUser.name},
         Address:${address.street}, ${address.number}
-        Phone:(${phone.ddd})${phone.phone}
+        Phone:(${phone.ddd})${phone.phone},
+        Birthday: ${newUser.birthday},
+        Age: ${newUser.age}
             `);
       });
     });
