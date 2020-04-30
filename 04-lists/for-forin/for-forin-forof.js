df3f05d0c74edb950151c5e4459227f2370d8a03
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2020-04-30 20:34:06 +0100

    Running lists with For-ForIn-ForOf.

--- 04-lists/for-forin/for-forin-forof.js
+++ 04-lists/for-forin/for-forin-forof.js
@@ -2,12 +2,26 @@ const service = require('./service.js')
 
 async function main(){
     try {
-        const result = await service.getPokemons()
-        
-        console.log(result);        
+        // The list of Pokemons in JSON format.
+        const resultJSON = await service.getPokemons()
 
+        // For to get only the names.
+        // for(let i=0; i < resultJSON.results.length; i++){
+        //     console.log(resultJSON.results[i].name);
+        // }
+
+        // // ForIn to get only the names.
+        // for (const i in resultJSON.results) {
+        //     console.log(resultJSON.results[i].name);
+        // }
+
+        // // ForOf to get only the names.
+        for (const pokemon of resultJSON.results) {
+            console.log(pokemon.name);
+        }
+    
     } catch (error) {
-        console.error('An error occured while consulting the list of Pokemons.');
+        console.error('An error occured while consulting the list of Pokemons.', error);
     }
 }
 
--- 04-lists/for-forin/service.js
+++ 04-lists/for-forin/service.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-const URL = 'https://api.pokemontcg.io/v1/cards'
+const URL = 'https://pokeapi.co/api/v2/pokemon?limit=100'
 
 async function getAllPokemons(){
   const response = await axios.get(URL)
